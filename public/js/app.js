c98e26b7b8e33a510bf8f73d101d9a204b573758
rishi dubey <rishi18454@gmail.com>
2021-06-22 21:26:30 +0530

    forecast now printing after fetch properly

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,26 @@
+
+
 const fetchForecast = async function (address) {
+  const forecast  = document.querySelector("#forecast");
+  const location = document.querySelector("#location");
+  
+
+  forecast.textContent = "Loading...";
+  location.textContent = "";
   const response = await fetch(`/weather?address=${address}`);
 
   const data = await response.json();
   if (data.Error) {
-    return console.log(data.Error);
+    forecast.textContent = data.Error;
+    return;
   }
-  console.log(data);
+  location.textContent = data.exactLocation;
+  forecast.textContent = data.forecast;
 };
 
 const handleFormSubmit = (e) => {
   e.preventDefault();
+  document.querySelector("#submit-btn").blur();
   const address = document.querySelector("#address");
   fetchForecast(address.value);
   address.value = "";
